bab31eb5f9259ae8a4a1da7d8e67be98ef0e7bec
R15Hi <rk.8969009@gmail.com>
2021-04-08 07:07:55 +0200

    official tag

--- kangos_RMX1851.mk
+++ kangos_RMX1851.mk
@@ -22,6 +22,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
   ro.kangos.maintainer=Rishi Roy \
   ro.kangos.cpu=SDM710
 
+#official
+KANGOS_BUILDTYPE := OFFICIAL
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
